17f7785caa307bb50e445bcca9adfce546cdeed6
Adner Ramos <adnerramos21@gmail.com>
2019-03-16 21:13:16 -0500

    Progress on action-wrapper to get the names to come in from the side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit, HostListener } from '@angular/core';
-import { TweenLite } from 'gsap/TweenMax';
+import { TweenLite, TweenMax } from 'gsap/TweenMax';
 
 @Component({
   selector: 'app-root',
@@ -12,21 +12,47 @@ export class AppComponent implements AfterViewInit {
 
   @HostListener('scroll', ['$event'])
   scrollHandler(event) {
-    const scrollPosition = event.srcElement.scrollTop;
 
-    const indicator = document.getElementById('indicator');
-    const verticalLine = document.getElementsByClassName('vertical-line');
+    const scrollPosition = event.srcElement.scrollTop,
+      positionLimitForIndicator = 252,
+      positionLimitForVerticalLine = 630,
+      indicator = document.getElementById('indicator'),
+      verticalLine = document.getElementsByClassName('vertical-line'),
+      first = document.getElementsByClassName('first'),
+      last = document.getElementsByClassName('last');
 
-    if (scrollPosition <= 252) {
+    let currentPositionFirstElem = 0,
+      currentPositionLastElem = 0;
+
+
+    if (scrollPosition <= positionLimitForIndicator) {
       TweenLite.to(indicator, 0, {
         strokeDashoffset: scrollPosition
       });
 
       verticalLine[0].setAttribute('style', 'height: 0');
-    } else {
+
+    } else if (scrollPosition > positionLimitForIndicator && scrollPosition <= positionLimitForVerticalLine) {
       TweenLite.to(verticalLine, 0, {
-        height: scrollPosition - 252
+        height: scrollPosition - positionLimitForIndicator
       });
+
+      first[0].setAttribute('style', 'transform: matrix(1, 0, 0, 1, -192, 0)');
+      last[0].setAttribute('style', 'transform: matrix(1, 0, 0, 1, 670, 0)');
+
+    } else if (scrollPosition > positionLimitForVerticalLine && scrollPosition <= 1000) {
+      console.log(scrollPosition);
+      currentPositionFirstElem = (scrollPosition - positionLimitForVerticalLine) - 192;
+      currentPositionLastElem = 670 - (scrollPosition - positionLimitForVerticalLine);
+
+      TweenLite.to(first, 0, {
+        transform: `matrix(1, 0, 0, 1, ${currentPositionFirstElem}, 0)`
+      });
+
+      TweenLite.to(last, 0, {
+        transform: `matrix(1, 0, 0, 1, ${currentPositionLastElem}, 0)`
+      });
+
     }
 
   }
